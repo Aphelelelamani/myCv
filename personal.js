4561413b11028ba545192846b4744528c54700bd
Aphelelelamani <87131320+Aphelelelamani@users.noreply.github.com>
2021-09-15 22:05:24 +0200

    Add files via upload

---
+++ personal.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#thumb').hover(function() {
+$(this).stop().animate({
+top: -50
+}, 900, "easeOutBounce");
+}, function() {
+$(this).stop().animate({
+top: 0
+}, 900, "easeOutBounce");
+});
+})
